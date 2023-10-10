c80df61d2fb86d05ff3408dffc553619e947604e
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-10-10 23:43:35 +0700

    Update build-kmods.sh

--- config/scripts/build-kmods.sh
+++ config/scripts/build-kmods.sh
@@ -5,13 +5,13 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-rpm-ostree install kmod-v4l2loopback
-
-ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
-
 KERNEL_NAME='kernel-cachyos-bore-eevdf'
 ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+rpm-ostree install \
+    akmod-v4l2loopback-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod v4l2loopback
+modinfo /usr/lib/modules/${KERNEL}/extra/v4l2loopback/v4l2loopback.ko.xz > /dev/null \
+|| (find /var/cache/akmods/v4l2loopback/ -name \*.log -print -exec cat {} \; && exit 1)
