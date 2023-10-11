46b0f222495f85d122fef67d2b3311827160b494
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-10-11 11:39:12 +0700

    test kmods script using vanilla kernel

--- config/scripts/build-kmods.sh
+++ config/scripts/build-kmods.sh
@@ -5,7 +5,7 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-KERNEL_NAME='kernel-cachyos-lts'
+KERNEL_NAME='kernel-core'
 ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
