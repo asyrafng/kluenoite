27290695ef21ab5333a8c88f3e27de21f84892cf
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-10-12 06:53:10 +0700

    revert installed kernel packages

--- config/scripts/replace_kernel_cachy_lts.sh
+++ config/scripts/replace_kernel_cachy_lts.sh
@@ -7,20 +7,14 @@ FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
 INSTALLED_KERNEL_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^kernel(?!-tools).*')"
 
 printf "### Fedora version ###\n$FEDORA_VERSION\n\n"
-# printf "### Packages to be replaced ###\n$INSTALLED_KERNEL_PACKAGES\n\n"
+printf "### Packages to be replaced ###\n$INSTALLED_KERNEL_PACKAGES\n\n"
 sleep 2
 
 # Use rpm-ostree's cliwrap to allow dracut to run on the container and generate
 # an initramfs.
 ### COMMENT OUT BELOW LINE IF USING LOCALLY ###
 rpm-ostree cliwrap install-to-root / && \
-rpm-ostree override remove kernel-modules-core \
-kernel-core \
-kernel-modules \
-kernel \
-kernel-modules-extra \
-kernel-devel \
-kernel-devel-matched --install=kernel-cachyos-lts 
+rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-cachyos-lts 
 rpm-ostree install kernel-cachyos-lts-devel-matched kernel-cachyos-lts-devel
 # rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-specified
 # rpm-ostree override replace "https://download.copr.fedorainfracloud.org/results/bieszczaders/kernel-cachyos/fedora-38-x86_64/06407061-kernel-cachyos-bore-eevdf/kernel-cachyos-bore-eevdf-6.5.3-cbe3.0.fc38.x86_64.rpm"
