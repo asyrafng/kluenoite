b8f60f4002a96889640ba9b4b4412648be7e7639
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-10-11 20:48:51 +0700

    Update build-kmods.sh

--- config/scripts/build-kmods.sh
+++ config/scripts/build-kmods.sh
@@ -5,11 +5,12 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
+
 KERNEL_NAME='kernel-cachyos-lts'
 ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+chmod 1777 /tmp /var/tmp
 akmods --force --kernels "${KERNEL}" --kmod v4l2loopback
-modinfo /usr/lib/modules/${KERNEL}/extra/v4l2loopback/v4l2loopback.ko.xz > /dev/null \
-|| (find /var/cache/akmods/v4l2loopback/ -name \*.log -print -exec cat {} \; && exit 1)
