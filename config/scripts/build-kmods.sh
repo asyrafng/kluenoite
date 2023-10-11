25f316db3842d34a35f539bc71a1b65cb1560309
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-10-11 12:16:30 +0700

    Update build-kmods.sh

--- config/scripts/build-kmods.sh
+++ config/scripts/build-kmods.sh
@@ -10,8 +10,6 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-rpm-ostree install \
-    akmod-v4l2loopback-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod v4l2loopback
 modinfo /usr/lib/modules/${KERNEL}/extra/v4l2loopback/v4l2loopback.ko.xz > /dev/null \
 || (find /var/cache/akmods/v4l2loopback/ -name \*.log -print -exec cat {} \; && exit 1)
