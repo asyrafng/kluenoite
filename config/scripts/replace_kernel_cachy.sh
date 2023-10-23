142508e21ab484f2d19b7e8e6b9d4f14958a3122
asyrafng <145214775+asyrafng@users.noreply.github.com>
2023-10-23 11:16:01 +0700

    experiment with realtime kernel

--- config/scripts/replace_kernel_cachy.sh
+++ config/scripts/replace_kernel_cachy.sh
@@ -14,7 +14,7 @@ sleep 2
 # an initramfs.
 ### COMMENT OUT BELOW LINE IF USING LOCALLY ###
 rpm-ostree cliwrap install-to-root / && \
-rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-cachyos-bore-eevdf 
+rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-cachyos-bore-eevdf-rt
 # rpm-ostree install kernel-cachyos-bore-eevdf-devel-matched kernel-cachyos-bore-eevdf-devel
 # rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-specified
 # rpm-ostree override replace "https://download.copr.fedorainfracloud.org/results/bieszczaders/kernel-cachyos/fedora-38-x86_64/06407061-kernel-cachyos-bore-eevdf/kernel-cachyos-bore-eevdf-6.5.3-cbe3.0.fc38.x86_64.rpm"
